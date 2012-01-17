0324a2f290e846380054c29be9ce20f758402a8e
susnow <susnowngacn@gmail.com>
2012-01-17 22:54:29 +0800

    create branch,first commit

---
+++ AutoLFG/config.lua
@@ -0,0 +1,13 @@
+﻿--susnow
+local addon,ns = ...
+local cfg = CreateFrame("Frame")
+
+cfg.igonoreOld = true
+local media = "Interface\\AddOns\\AutoLFG\\"
+local bd = "Interface\\Buttons\\WHITE8x8"
+cfg.countSound1 = media.."sounds\\CountDown1.ogg"
+cfg.countSound2 = media.."sounds\\CountDown2.ogg"
+cfg.bar = media.."textures\\bar.tga"
+cfg.backdrop = {edgeFile = bd,bgFile = bd, edgeSize = 1}
+
+ns.cfg = cfg
